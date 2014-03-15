47e16ea603d202003c24325f9eb17c813d08bbee
cardo89 <cardo89@gmail.com>
2014-03-15 19:02:08 +0100

    Added reconstruction
    —> cwt substituted by cwt to obtained the needed struct
    —> reconstruction made with icwtft
    —> Reconstruction is not perfect
    Note: matlab doesn’t perform perfect reconstruction of cwt

--- testWavelets.m
+++ testWavelets.m
@@ -4,6 +4,14 @@ close all
 
 [x, fs, Nbits] = wavread('robinhood.wav');
 
-X = cwtft(x(:,1),'scales',1:32,'wavelet','mexh');
+x = x(:,1);
+X = cwtft(x(:,1),'scales',1:256,'wavelet','mexh');
 
-Xrec = icwtlin(X);
+xRec = icwtft(X);
+
+
+% % Normilizing recontructed signal in order to have the same dinamic of x
+% xRec = (xRec - min(xRec))/(max(xRec) -min(xRec)); % Normalizing between [0;1]
+% xRec = xRec*(max(x) - min(x)) + min(x); %Rescaling the recontructe signal to thge same dynamics of x
+
+plot(x(:,1)), hold on, plot(xRec,'r');
